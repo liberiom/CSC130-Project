fd931c03a65219a1640a4eb22aec244dabb3ab89
Liberio Mukdani <liberiomukdani@csus.edu>
2022-03-27 22:59:37 -0700

    Checkpoint #3

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -12,7 +12,7 @@ import timer.stopWatchX;
 
 public class Main{
 	// Fields (Static) below...
-	public static stopWatchX timer = new stopWatchX(50);
+	public static stopWatchX timer = new stopWatchX(100);
 	public static ArrayList<spriteInfo> sprites = new ArrayList<>();
 	public static int currentSpriteIndex = 0;
 	// End Static fields...
@@ -27,24 +27,25 @@ public class Main{
 		for (int i = -128; i < (1280 + 128); i += 8) {
 			spriteInfo stmp = new spriteInfo(new Vector2D(i, 200), "frame" + (currentSpriteIndex + 1));
 			sprites.add(stmp);
-			if(currentSpriteIndex == 8) {
+			if(currentSpriteIndex == 7) {
 				currentSpriteIndex = 0;
 			} else {
 				currentSpriteIndex++;
 			}
-		}
-		currentSpriteIndex = 0;
-	}
+		} 
+		currentSpriteIndex = 0; 
+	} 
 	
 	/* This is your access to the "game loop" (It is a "callback" method from the Control class (do NOT modify that class!))*/
 	public static void update(Control ctrl) {
 		ctrl.addSpriteToFrontBuffer(sprites.get(currentSpriteIndex).getCoords().getX(), sprites.get(currentSpriteIndex).getCoords().getY(), sprites.get(currentSpriteIndex).getTag());
 		if (timer.isTimeUp()) {
-			if (currentSpriteIndex == sprites.size()) {
+			timer.resetWatch();
+			if (currentSpriteIndex == sprites.size() - 1) {
 				currentSpriteIndex = 0;
 			} else {
 				currentSpriteIndex++;
 			}
-		}
+		}	
 	}
 }

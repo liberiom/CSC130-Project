89c99ed507f28622cc516b30dc104cc85d166cdf
liberiom <liberiomukdani@csus.edu>
2022-03-27 21:06:28 -0700

    Attempt 1 at Checkpoint #3

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,20 +1,20 @@
 package Main;
 
 import java.awt.Color;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
 import Data.Vector2D;
+import Data.spriteInfo;
 import logic.Control;
 import timer.stopWatchX;
 
 public class Main{
 	// Fields (Static) below...
-	public static boolean isImageDrawn = false;
 	public static stopWatchX timer = new stopWatchX(50);
-	public static Queue<Vector2D> vecs1 = new LinkedList<>();
-	public static Queue<Vector2D> vecs2 = new LinkedList<>();
-	public static Vector2D currentVec = new Vector2D(-100, -100);
+	public static ArrayList<spriteInfo> sprites = new ArrayList<>();
+	public static int currentSpriteIndex = 0;
 	// End Static fields...
 	
 	public static void main(String[] args) {
@@ -24,32 +24,27 @@ public class Main{
 	
 	/* This is your access to things BEFORE the game loop starts */
 	public static void start(){
-		// TODO: Code your starting conditions here...NOT DRAW CALLS HERE! (no addSprite or drawString)
 		for (int i = -128; i < (1280 + 128); i += 8) {
-			vecs1.add(new Vector2D(i, 200));
+			spriteInfo stmp = new spriteInfo(new Vector2D(i, 200), "frame" + (currentSpriteIndex + 1));
+			sprites.add(stmp);
+			if(currentSpriteIndex == 8) {
+				currentSpriteIndex = 0;
+			} else {
+				currentSpriteIndex++;
+			}
 		}
-		currentVec.setX(vecs1.peek().getX());
-		currentVec.setY(vecs1.peek().getY());
-		vecs1.remove();
+		currentSpriteIndex = 0;
 	}
 	
 	/* This is your access to the "game loop" (It is a "callback" method from the Control class (do NOT modify that class!))*/
 	public static void update(Control ctrl) {
-		// TODO: This is where you can code! (Starting code below is just to show you how it works)
-		ctrl.addSpriteToFrontBuffer(currentVec.getX(), currentVec.getY(), "greenguy");
+		ctrl.addSpriteToFrontBuffer(sprites.get(currentSpriteIndex).getCoords().getX(), sprites.get(currentSpriteIndex).getCoords().getY(), sprites.get(currentSpriteIndex).getTag());
 		if (timer.isTimeUp()) {
-			currentVec.setX(vecs1.peek().getX());
-			currentVec.setY(vecs1.peek().getY());
-			vecs2.add(vecs1.remove());
-			timer.resetWatch();
-			if (vecs1.isEmpty()) {
-				while(!vecs2.isEmpty()) {
-					vecs1.add(vecs2.remove());
-				}
+			if (currentSpriteIndex == sprites.size()) {
+				currentSpriteIndex = 0;
+			} else {
+				currentSpriteIndex++;
 			}
 		}
 	}
-	
-	// Additional Static methods below...(if needed)
-
 }

f8db947ff46d84a760544b62f908f66a19439f8a
liberiom <liberiomukdani@csus.edu>
2022-04-09 01:01:15 -0700

    Seeing if the HashMap works

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -2,21 +2,22 @@ package Main;
 
 import java.awt.Color;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.StringTokenizer;
 
 import Data.Vector2D;
 import Data.spriteInfo;
+import FileIO.EZFileRead;
 import logic.Control;
 import timer.stopWatchX;
 
 public class Main{
 	// Fields (Static) below...
-	public static stopWatchX timer = new stopWatchX(100);
-	public static ArrayList<spriteInfo> sprites = new ArrayList<>();
-	public static int currentSpriteIndex = 0;
-	// End Static fields...
 	
+	public static stopWatchX timer = new stopWatchX(100);
+	// End Static fields...
 	public static void main(String[] args) {
 		Control ctrl = new Control();				// Do NOT remove!
 		ctrl.gameLoop();							// Do NOT remove!
@@ -24,28 +25,26 @@ public class Main{
 	
 	/* This is your access to things BEFORE the game loop starts */
 	public static void start(){
-		for (int i = -128; i < (1280 + 128); i += 8) {
-			spriteInfo stmp = new spriteInfo(new Vector2D(i, 200), "frame" + (currentSpriteIndex + 1));
-			sprites.add(stmp);
-			if(currentSpriteIndex == 7) {
-				currentSpriteIndex = 0;
-			} else {
-				currentSpriteIndex++;
-			}
-		} 
-		currentSpriteIndex = 0; 
+		HashMap<String, String> map = new HashMap<>();
+		String raw = "", key, value;
+		StringTokenizer st = new StringTokenizer(raw, "*");
+		EZFileRead ezr = new EZFileRead("scripts.txt");
+		int lines = ezr.getNumLines();
+		for (int i = 0; i < lines; i++) {
+			raw = ezr.getLine(i);
+			key = st.nextToken();
+			value = st.nextToken();
+			map.put(key, value);
+		}
+		
+		// Debugging
+		for (int i = 0; i < map.size(); i++) {
+			System.out.println(map.get("string" + i));
+		}
 	} 
 	
 	/* This is your access to the "game loop" (It is a "callback" method from the Control class (do NOT modify that class!))*/
 	public static void update(Control ctrl) {
-		ctrl.addSpriteToFrontBuffer(sprites.get(currentSpriteIndex).getCoords().getX(), sprites.get(currentSpriteIndex).getCoords().getY(), sprites.get(currentSpriteIndex).getTag());
-		if (timer.isTimeUp()) {
-			timer.resetWatch();
-			if (currentSpriteIndex == sprites.size() - 1) {
-				currentSpriteIndex = 0;
-			} else {
-				currentSpriteIndex++;
-			}
-		}	
+		
 	}
 }

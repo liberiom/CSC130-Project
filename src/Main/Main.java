9f4c7b168e5b521724a5f7476c8fdf0ebc6405df
liberiom <liberiomukdani@csus.edu>
2022-04-28 12:32:46 -0700

    Collision Test

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -7,6 +7,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
+import Data.BoundingBox;
 import Data.Vector2D;
 import Data.spriteInfo;
 import FileIO.EZFileRead;
@@ -20,11 +21,15 @@ public class Main{
 	public static String trigger = "";
 	public static Vector2D startPosition = new Vector2D(500, 500);
 	public static Vector2D dummyVector2D = new Vector2D(0, 0);
+	private static Vector2D lastPosition = new Vector2D(0,0);
 	public static int speed = 10;
 	public static boolean visible = true;
 	public static boolean isRight = true;
 	public static Queue<spriteInfo> spritesRight = new LinkedList<spriteInfo>();
 	public static Queue<spriteInfo> spritesLeft = new LinkedList<spriteInfo>();
+	public static ArrayList<BoundingBox> boxes = new ArrayList<BoundingBox>();
+	private static spriteInfo playerStartingSprite = new spriteInfo(startPosition, "frame1");
+	public static BoundingBox playerBox = new BoundingBox(playerStartingSprite, speed);
 	
 	// End Static fields...
 	public static void main(String[] args) {
@@ -38,6 +43,13 @@ public class Main{
 			spritesRight.add(new spriteInfo(dummyVector2D, "frame" + i));
 			spritesLeft.add(new spriteInfo(dummyVector2D, "flippedframe" + i));
 		}
+		
+		// Player BoundingBox
+		BoundingBox playerBox = new BoundingBox(playerStartingSprite, 128);
+		
+		// Adding BoundingBoxes ArrayList
+		boxes.add(new BoundingBox(0, 18, 0, 11));
+		
 	} 
 	
 	/* This is your access to the "game loop" (It is a "callback" method from the Control class (do NOT modify that class!))*/
@@ -47,5 +59,34 @@ public class Main{
 		} else {
 			ctrl.addSpriteToFrontBuffer(startPosition.getX(), startPosition.getY(), spritesLeft.peek().getTag());
 		}
+		
+		/*
+		 * Checking the player's collision against walls, doors, enemies, and chests
+		 */
+		for (int i = 0; i < boxes.size(); i++) {
+			if (checkCollision(playerBox, boxes.get(i))) {
+				lastPosition.setX(startPosition.getX());
+				lastPosition.setY(startPosition.getY());
+				reboundPlayer(lastPosition);
+			}
+		}
+		
 	}
+	
+	private static boolean checkCollision(BoundingBox box1, BoundingBox box2) {
+		final boolean COLLISION_DETECTED = (box1.getX1() > box2.getX2()) 
+				|| (box1.getX2() < box2.getX1()) 
+				|| (box1.getY1() > box2.getY2())
+				|| (box1.getY2() < box2.getY1());
+		
+		if (COLLISION_DETECTED) {
+			return true;
+		}
+		return false;
+	}
+	
+	private static void reboundPlayer(Vector2D lastPosition) {
+		startPosition = lastPosition;
+	}
+	
 }

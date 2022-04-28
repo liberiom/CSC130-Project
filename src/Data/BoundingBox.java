64c4adff4e0cdea6a7546b8bbb92d3a6c404b2ae
liberiom <liberiomukdani@csus.edu>
2022-04-28 15:38:20 -0700

    Changed BoundingBox

--- src/Data/BoundingBox.java
+++ src/Data/BoundingBox.java
@@ -46,18 +46,18 @@ public class BoundingBox {
 	}
 	
 	public void setX1(int x1){
-		x1 = this.x1;
+		this.x1 = x1;
 	}
 	
 	public void setX2(int x2){
-		x2 = this.x2;
+		this.x2 = x2;
 	}
 	
 	public void setY1(int y1){
-		y1 = this.y1;
+		this.y1 = y1;
 	}
 	
 	public void setY2(int y2){
-		y2 = this.y2;
+		this.y2 = y2;
 	}
 }
